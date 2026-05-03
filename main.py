f5976f8960a518a2d1810b8eb5fdb3ef66a14c87
Rafli Yusuf <rafliyusuf.s26@gmail.com>
2026-05-03 21:19:54 +0700

    Add comments to clarify number guessing game
    Added comments to explain the game logic and user prompts.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
 
-random_num = random.randint(1, 100)
+random_num = random.randint(1, 100) # It will select random number from 1 - 100
 
 while True:
-    ask = input("Do you want to play? (y/n): ").lower()
-    if ask == "y":
-        print("Great! Let's start.")
+    ask = input("Do you want to play? (y/n): ").lower() # Asking if you want to play game
+    if ask == "y": 
+        print("Great! Let's start.") # The game start if you type 'y'
         try:
             guess = int(input("Guess the number: "))
-            if guess < random_num:
+            if guess < random_num: 
                 print("Too low! Try again.")
             elif guess > random_num:
                 print("Too high! Try again.")
@@ -16,9 +16,9 @@ while True:
                 print("You got it!")
                 break
         except ValueError:
-            print("Please Enter Valid Number")
+            print("Please Enter Valid Number") 
     elif ask == 'n':
-        print("Okay, maybe next time!")
+        print("Okay, maybe next time!") # The game stop if you type 'n'
         break
     else:
-        print("Invalid input. Please enter 'y' or 'n'.")
+        print("Invalid input. Please enter 'y' or 'n'") # Error if type random than 'y' or 'n'
